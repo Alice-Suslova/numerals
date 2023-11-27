cb6bd7afee745027c15362080510f4f452aa9976
alica_suslova <alisasuslova1408@gmail.com>
2023-11-28 00:40:46 +0300

    main.kt changed_1

--- src/test/kotlin/main.kt
+++ src/test/kotlin/main.kt
@@ -1,2 +1,12 @@
-class main {
+package ru.netology
+
+fun main() {
+
+    //val likes = 63
+    val likes = 101
+    if ((likes % 10 == 1) && (likes !== 11)) {
+        println("Нравится $likes человеку")
+    } else {
+        println("Нравится $likes людям")
+    }
 }
