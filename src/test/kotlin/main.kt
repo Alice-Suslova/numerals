93fe5f583649ad7ed3a7d20dde6ef31775c8cd9c
alica_suslova <alisasuslova1408@gmail.com>
2023-11-28 22:24:38 +0300

    chenged 28.11.23

--- src/test/kotlin/main.kt
+++ src/test/kotlin/main.kt
@@ -2,10 +2,18 @@ package ru.netology
 
 fun main() {
 
-    //val likes = 63
     val likes = 101
-    if ((likes % 10 == 1) && (likes !== 11)) {
-        println("Нравится $likes человеку")
+
+    var last = likes % 10
+    var del = likes / 10
+    var previous = del % 10
+
+    if (last == 1) {
+        if (previous == 1) {
+            println("Нравится $likes людям")
+        } else {
+            println("Нравится $likes человеку")
+        }
     } else {
         println("Нравится $likes людям")
     }
